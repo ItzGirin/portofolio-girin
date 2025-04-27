53b6a55ed4bd9b8205bfadfc40fa45e045b7e3ec
ItzGirin <girindhra24@gmail.com>
2025-04-27 08:47:58 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Pesan Anda telah dikirim!');
-    this.reset();  // Reset form setelah pengiriman
+  event.preventDefault();
+  alert('Pesan Anda telah dikirim!');
+  this.reset();  // Reset form setelah pengiriman
 });
