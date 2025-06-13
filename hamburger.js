d8c84db89429e8610d1f45229029711146939cf5
ItzGirin <girindhra24@gmail.com>
2025-06-13 09:59:17 +0700

    Add files via upload

--- hamburger.js
+++ hamburger.js
@@ -1,9 +1,3 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  alert('Pesan Anda telah dikirim!');
-  this.reset();  // Reset form setelah pengiriman
-});
-
 document.addEventListener('DOMContentLoaded', function () {
   const menuToggle = document.getElementById('menu-toggle');
   const navLinks = document.querySelector('.main-nav .nav-links');
